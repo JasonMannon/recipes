a87e496ddfd3b1a420e6e5cf471adfbb14990ad6
Jason Mannon & Due Ho <student@epicodus.com>
2014-04-03 14:56:47 -0700

    Ratings incorporated

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.all.order(rating: :desc)
     render('recipes/index.html.erb')
   end
 
@@ -36,7 +36,8 @@ class RecipesController < ApplicationController
   def update
     @recipe = Recipe.find(params[:id])
     if @recipe.update(:name => params[:name],
-                      :description => params[:description])
+                      :description => params[:description],
+                      :rating => params[:rating])
       redirect_to("/recipes/#{@recipe.id}")
     else
       render('recipes/edit.html.erb')
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140403174724) do
+ActiveRecord::Schema.define(version: 20140403212401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140403174724) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "rating"
   end
 
   create_table "recipes_tags", force: true do |t|
